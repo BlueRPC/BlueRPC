77e48ba21ea12dbfe54067922d2be8bf1de65273
drosoCode <contact@thomasz.me>
2023-08-16 21:39:56 +0200

    fix cert generation

--- client/bluerpc_client/crypto.py
+++ client/bluerpc_client/crypto.py
@@ -1,4 +1,5 @@
 import datetime
+import ipaddress
 
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes, serialization
@@ -13,6 +14,7 @@ def create_certs(
     validity: datetime.timedelta = datetime.timedelta(weeks=5000),
     key_size=2048,
     signing_key: rsa.RSAPrivateKey | None = None,
+    issuer_cert: x509.Certificate | None = None
 ) -> (rsa.RSAPrivateKey, x509.Certificate):
     """
     Helper to create certificates for bluerpc
@@ -24,6 +26,7 @@ def create_certs(
         validity: the period of validity for this cert
         key_size: the key size
         signing_key: the key to be used to sign the certificate, use the key from a CA or leave empty to create a self-signed cert
+        issuer_cert: the certificated corresponding to the signing key
     Returns:
         (rsa.RSAPrivateKey, x509.Certificate): a tuple of the private key and the certificate
     """
@@ -38,7 +41,9 @@ def create_certs(
             x509.NameAttribute(NameOID.COMMON_NAME, common_name),
         ]
     )
-    cert = (
+    if issuer_cert:
+        issuer = issuer_cert.subject
+    builder = (
         x509.CertificateBuilder()
         .subject_name(subject)
         .issuer_name(issuer)
@@ -46,8 +51,14 @@ def create_certs(
         .serial_number(x509.random_serial_number())
         .not_valid_before(datetime.datetime.now(datetime.timezone.utc))
         .not_valid_after(datetime.datetime.now(datetime.timezone.utc) + validity)
-        .sign(signing_key or key, hashes.SHA256())
     )
+    try:
+        ip = ipaddress.ip_address(common_name)
+        ext = x509.SubjectAlternativeName([x509.IPAddress(ip)])
+        builder = builder.add_extension(ext, True)
+    except ValueError:
+        pass
+    cert = builder.sign(signing_key or key, hashes.SHA256())
     return (key, cert)
 
 
@@ -64,7 +75,7 @@ def serialize_certs(data: rsa.RSAPrivateKey | x509.Certificate) -> bytes:
         return data.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption,
+            encryption_algorithm=serialization.NoEncryption(),
         )
     else:
         return data.public_bytes(serialization.Encoding.PEM)
